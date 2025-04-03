9a2ba748b724bff9211949d823b6080988843233
Marlon Bezerra <123117179+marlonbezerradossantos@users.noreply.github.com>
2025-04-03 18:08:06 -0300

    estudos do dia 03/04/25

--- dom-interactive-rating-component-main/script/index.js
+++ dom-interactive-rating-component-main/script/index.js
@@ -1,5 +1,13 @@
 "use strict"
 
+const numbers = document.querySelectorAll('.rating__number')
+
+
 function guardaValor(n) {
+    for(let n of numbers) {
+        n.style.backgroundColor = "#272e38"
+        
+    }
+    numbers[n-1].style.backgroundColor = "#f97813"
     sessionStorage.setItem("rate", n)
 }
